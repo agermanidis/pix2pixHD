c9cdf0b256ec0953abd8daf0fc67718803cd7769
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-07-28 13:48:21 -0400

    multiple images first try

--- multiple_images.py
+++ multiple_images.py
@@ -1,7 +1,8 @@
-# Run inference over a single image
+# Run inference over a multiple image
 # =================
 import os
 from PIL import Image
+import argparse
 import cv2
 from collections import OrderedDict
 from torch.autograd import Variable
@@ -15,6 +16,7 @@ import torch
 from data.base_dataset import get_params, get_transform
 from torchvision import models, transforms
 
+# Model Options that match the training
 opt = TestOptions().parse(save=False)
 opt.nThreads = 1
 opt.batchSize = 1
@@ -26,28 +28,35 @@ opt.use_features = False
 opt.no_instance = True
 opt.label_nc = 0
 
+# Load the model
 model = create_model(opt)
-raw_img = Image.open("imgs/lateshow_pose.jpg")
 
-normalize = transforms.Normalize(
-   mean=[0.485, 0.456, 0.406],
-   std=[0.229, 0.224, 0.225]
-)
 
-preprocess = transforms.Compose([
-   transforms.Scale(256),
-   transforms.CenterCrop(224),
-   transforms.ToTensor(),
-   normalize
-])
+def main():
+  print('Running pix2pixHD over all images in %s' % args.images_dir)
+  images = [img for img in os.listdir(args.images_dir) if img.endswith(".%s" % args.images_format)]
 
-params = get_params(opt, raw_img.size)
-transform_label = get_transform(opt, params, method=Image.NEAREST, normalize=False)
-label_tensor = transform_label(raw_img)
-label_tensor = label_tensor.unsqueeze(0)
+  for image in images:
+    raw_img = Image.open(os.path.join(args.images_dir, image))
+    params = get_params(opt, raw_img.size)
+    transform_label = get_transform(opt, params, method=Image.NEAREST, normalize=False)
+    label_tensor = transform_label(raw_img)
+    label_tensor = label_tensor.unsqueeze(0)
+    # Get fake image
+    generated = model.inference(label_tensor, None)
+    # Save img
+    im = util.tensor2im(generated.data[0])
+    im_pil = Image.fromarray(im)
+    im_pil.save(os.path.join(args.output_dir, image))
 
-generated = model.inference(label_tensor, None)
-im = util.tensor2im(generated.data[0])
-im_pil = Image.fromarray(im)
-im_pil.save('outputs/test.jpg')
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-m', '--model', dest='model', default='lateshow', type=str, help='The model to use')
+  parser.add_argument('--images_dir', dest='images_dir', type=str, default='.', help='Path of images to use')
+  parser.add_argument('-img_f', '--images_format', dest='images_format', type=str, default='jpg', help='Format of images')
+  parser.add_argument('-vid_f', '--video_format', dest='video_format', type=str, default='mp4', help='Format of video')
+  parser.add_argument('-o', '--output_dir', dest='output_dir', type=str, default='./outputs', help='Output directory')
+  args = parser.parse_args()
+
+  main()
 
