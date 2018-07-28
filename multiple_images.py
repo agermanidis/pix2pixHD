5f92ef4d7b6805890e4cd808bdd7adb0d3057cf4
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-07-28 15:45:01 -0400

    add video

--- multiple_images.py
+++ multiple_images.py
@@ -17,6 +17,9 @@ class CustomOptions(TestOptions):
    self.parser.add_argument('-img_f', '--images_format', dest='images_format', type=str, default='jpg', help='Format of')
    self.parser.add_argument('-vid_f', '--video_format', dest='video_format', type=str, default='mp4', help='Format of v')
    self.parser.add_argument('-o', '--output_dir', dest='output_dir', type=str, default='./outputs', help='Output direct')
+   self.parser.add_argument('-v_name', '--video_name', dest='video_name', type=str, default='output', help='Video name')
+   self.parser.add_argument('-v_f', '--video_format', dest='video_format', type=str, default='.mp4', help='Video Format')
+   self.parser.add_argument('-make_video', '--make_video', dest='make_video', type=bool, default=True, help='Toggle video making')
 
 # Model Options that match the training
 opt = CustomOptions().parse(save=False)
@@ -49,6 +52,21 @@ def main():
     im = util.tensor2im(generated.data[0])
     im_pil = Image.fromarray(im)
     im_pil.save(os.path.join(opt.output_dir, image))
+
+  if opt.make_video:
+    print('Making video with %s' % opt.output_dir)
+    images = [img for img in os.listdir(opt.output_dir) if img.endswith(".%s" % opt.images_format)]
+    frame = cv2.imread(os.path.join(args.output_dir, images[0]))
+    height, width, layers = frame.shape
+
+    video = cv2.VideoWriter(os.path.join(opt.output_dir, opt.video_name + "." + opt.video_format), -1, args.fps, (width,height))
+
+    for image in images:
+      video.write(cv2.imread(os.path.join(opt.output_dir, image)))
+
+    cv2.destroyAllWindows()
+    video.release()
+    print('Done! Video ready.')
 
 if __name__ == '__main__':
   main()
