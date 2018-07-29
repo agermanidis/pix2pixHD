d726b9532ecec461ce192e8272360ce15ea2db1c
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-07-28 22:24:19 -0400

    set ports in docker

--- runServer.sh
+++ runServer.sh
@@ -1,4 +1,5 @@
 nvidia-docker run --rm -it -v /home/paperspace/pix2pixHD:/workspace/pix2pixHD \
   -v /storage:/workspace/storage \
+  -p 23100:23100 \
   --shm-size 16G \
   pix2pixhdserver:latest bash
--- server.py
+++ server.py
@@ -2,22 +2,23 @@
 # Run inference over a single image
 # =================
 import os
+import json
+import base64
+import common
+import io
+import numpy as np
+import gevent.monkey
+gevent.monkey.patch_all()
 from PIL import Image
+from flask import Flask, jsonify
+from flask_cors import CORS
+from flask_socketio import SocketIO, emit
 import cv2
 from options.test_options import TestOptions
 from models.models import create_model
 import util.util as util
 import torch
 from data.base_dataset import get_params, get_transform
-import json
-import base64
-import common
-import io
-import numpy as np
-from PIL import Image
-from flask import Flask, jsonify
-from flask_cors import CORS
-from flask_socketio import SocketIO, emit
 
 # Model Options that match the training
 opt = TestOptions().parse(save=False)
@@ -52,17 +53,37 @@ def toRGB(image):
   return cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
 
 def main(input_img):
-  image = stringToImage(input_img)
-  params = get_params(opt, image.size)
-  transform_label = get_transform(opt, params, method=Image.NEAREST, normalize=False)
-  label_tensor = transform_label(raw_img)
-  label_tensor = label_tensor.unsqueeze(0)
-  generated = model.inference(label_tensor, None)
-  im = util.tensor2im(generated.data[0])
-  im_pil = Image.fromarray(im)
-  buffer = io.BytesIO()
+  #raw_img = Image.open("imgs/lateshow/lateshow_pose.jpg")
+  #params = get_params(opt, raw_img.size)
+  #transform_label = get_transform(opt, params, method=Image.NEAREST, normalize=False)
+  #label_tensor = transform_label(raw_img)
+  #label_tensor = label_tensor.unsqueeze(0)
+
+  # Get fake image
+  #generated = model.inference(label_tensor, None)
+
+  # Save img
+  #im = util.tensor2im(generated.data[0])
+  #im_pil = Image.fromarray(im)
+  #buffer = io.BytesIO()
   #im_pil.save(buffer, format='JPEG')
-  return base64.b64encode(buffer.getvalue())
+  #return base64.b64encode(raw_img)
+
+  raw = cv2.imread("imgs/lateshow/lateshow_pose.jpg")
+  retval, buffer = cv2.imencode('.jpg', raw)
+  jpg_as_text = base64.b64encode(buffer)
+  return jpg_as_text
+
+  #image = stringToImage(input_img[input_img.find(",")+1:])
+  #img = toRGB(image)
+  #params = get_params(opt, image.size)
+  #transform_label = get_transform(opt, params, method=Image.NEAREST, normalize=False)
+  #label_tensor = transform_label(image)
+  #label_tensor = label_tensor.unsqueeze(0)
+  #generated = model.inference(label_tensor, None)
+  #im = util.tensor2im(generated.data[0])
+  #retval, buffer = cv2.imencode('.jpg', im)
+  #return base64.b64encode(buffer)
 
 # --- 
 # Server Routes
